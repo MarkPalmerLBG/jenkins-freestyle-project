16458582a2a376515441ee6d58f26cc401e5b62b
MarkPalmerLBG <168431274+MarkPalmerLBG@users.noreply.github.com>
2024-05-08 15:58:02 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
 echo 'Hello world!'
+sleep 1
+echo '1 second'
+sleep 1
+echo '2 seconds'
+sleep 1
+echo '3 seconds'
+sleep 1
+echo '4 seconds'
+sleep 1
+echo '5 seconds'
 echo 'this build has completed!!'
